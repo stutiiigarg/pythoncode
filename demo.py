171fa4b43591278046a235c85db4fb75b7253e1e
stutiiigarg <126191203+stutiiigarg@users.noreply.github.com>
2024-02-01 13:39:14 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,11 @@
+
+num1 = float(input("Enter the first number: "))
+
+
+num2 = float(input("Enter the second number: "))
+
+
+result = num1 + num2
+
+
+print("The sum of", num1, "and", num2, "is", result)
